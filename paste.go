3186efbe4d913507e744720d92e1ab2f20c8c588
NBens <benshaqi.nizar@gmail.com>
2018-09-05 13:29:40 +0100

    Comments

--- paste.go
+++ paste.go
@@ -2,68 +2,60 @@ package main
 
 import (
 	"errors"
-	"strings"
+	"io/ioutil"
 	"net/http"
 	"net/url"
-	"io/ioutil"
 	"strconv"
+	"strings"
 )
 
-
-/*
-* Paste Structure
-* Paste.privacy: 0=public 1=unlisted 2=private
-* Paste.expirationDate:  // N: Never, 5M: 5 Minutes, 5H: 5Hours, 2D: 2 Days
-* Paste.format: Syntax highlight format
-* check https://pastebin.com/api/ for more details about privacy and expirationDate.
-*/
-
+// Paste Structure
+// Paste.privacy: 0=public 1=unlisted 2=private
+// Paste.expirationDate:  // N: Never, 5M: 5 Minutes, 5H: 5Hours, 2D: 2 Days
+// Paste.format: Syntax highlight format
+// check https://pastebin.com/api/ for more details about privacy and expirationDate.
 
 type Paste struct {
-	apiKey string         // Required Parameter
-	title string          // Optional Parameter
-	text string           // Required Parameter
-	privacy int           // Optional Parameter
-	format string         // Optional Parameter
+	apiKey         string // Required Parameter
+	title          string // Optional Parameter
+	text           string // Required Parameter
+	privacy        int    // Optional Parameter
+	format         string // Optional Parameter
 	expirationDate string // Optional Parameter
-	pasteID string        // Optional Parameter (Required to delete a paste)
+	pasteID        string // Optional Parameter (Required to delete a paste)
 }
 
-/*
-* Method responsible for creating a new paste, returns the paste's url (string)
-*/
-
-
+// NewPaste Method is responsible for creating a new paste, returns the paste's url (string)
 func (p *Paste) NewPaste() (string, error) {
 
 	apiOption := "paste"
 
-	data_values := url.Values{
-		"api_dev_key": {p.apiKey},
-		"api_option": {apiOption},
-		"api_paste_code": {p.text},
-		"api_paste_name": {p.title},
-		"api_expire_date": {p.expirationDate},
-		"api_paste_format": {p.format},
+	dataValues := url.Values{
+		"api_dev_key":       {p.apiKey},
+		"api_option":        {apiOption},
+		"api_paste_code":    {p.text},
+		"api_paste_name":    {p.title},
+		"api_expire_date":   {p.expirationDate},
+		"api_paste_format":  {p.format},
 		"api_paste_private": {strconv.Itoa(p.privacy)},
 	}
-	data, httpResponseError := http.PostForm(API_POST, data_values)
+	data, httpResponseError := http.PostForm(APIPost, dataValues)
 
 	if strings.TrimSpace(p.text) == "" {
 		err := errors.New("Empty text was given")
 		return "", err
-	} 
+	}
 
 	if httpResponseError != nil {
 		return "", httpResponseError
 	}
-	
+
 	if data.StatusCode != 200 {
 		return "", errors.New("Couldn't create the paste: " + strconv.Itoa(data.StatusCode))
 	}
 
 	defer data.Body.Close()
-	
+
 	returnedData, err := ioutil.ReadAll(data.Body)
 	if err != nil {
 		return "", err
@@ -72,32 +64,32 @@ func (p *Paste) NewPaste() (string, error) {
 
 }
 
-
+// PrivatePaste method creates a new private paste, it needs the userKey (generated from username and password).
 func (p *Paste) PrivatePaste(userKey string) (string, error) {
 
 	apiOption := "paste"
-	data_values := url.Values{
-		"api_dev_key": {p.apiKey},
-		"api_user_key": {userKey},
-		"api_option": {apiOption},
-		"api_paste_code": {p.text},
-		"api_paste_name": {p.title},
-		"api_expire_date": {p.expirationDate},
-		"api_paste_format": {p.format},
+	dataValues := url.Values{
+		"api_dev_key":       {p.apiKey},
+		"api_user_key":      {userKey},
+		"api_option":        {apiOption},
+		"api_paste_code":    {p.text},
+		"api_paste_name":    {p.title},
+		"api_expire_date":   {p.expirationDate},
+		"api_paste_format":  {p.format},
 		"api_paste_private": {"2"},
 	}
-	data, httpResponseError := http.PostForm(API_POST, data_values)
+	data, httpResponseError := http.PostForm(APIPost, dataValues)
 
 	if httpResponseError != nil {
 		return "", httpResponseError
 	}
-	
+
 	if data.StatusCode != 200 {
 		return "", errors.New("Couldn't list the pastes: " + strconv.Itoa(data.StatusCode))
 	}
 
 	defer data.Body.Close()
-	
+
 	returnedData, err := ioutil.ReadAll(data.Body)
 	if err != nil {
 		return "", err
@@ -107,28 +99,28 @@ func (p *Paste) PrivatePaste(userKey string) (string, error) {
 
 }
 
-
+// Delete method deletes a paste using the userKey and the pasteID
 func (p *Paste) Delete(userKey string) (string, error) {
 
 	apiOption := "delete"
-	data_values := url.Values{
-		"api_dev_key": {p.apiKey},
-		"api_user_key": {userKey},
-		"api_option": {apiOption},
+	dataValues := url.Values{
+		"api_dev_key":   {p.apiKey},
+		"api_user_key":  {userKey},
+		"api_option":    {apiOption},
 		"api_paste_key": {p.pasteID},
 	}
-	data, httpResponseError := http.PostForm(API_POST, data_values)
+	data, httpResponseError := http.PostForm(APIPost, dataValues)
 
 	if httpResponseError != nil {
 		return "", httpResponseError
 	}
-	
+
 	if data.StatusCode != 200 {
 		return "", errors.New("Couldn't delete the paste: " + strconv.Itoa(data.StatusCode))
 	}
 
 	defer data.Body.Close()
-	
+
 	returnedData, err := ioutil.ReadAll(data.Body)
 	if err != nil {
 		return "", err
--- user.go
+++ user.go
@@ -1,80 +1,83 @@
 package main
 
 import (
-	"net/http"
-	"net/url"
 	"errors"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"strconv"
 )
 
+// APILogin: Url for the login api (which uses username and password to generate the user key)
+// APIPost: Url for the pastes api, it is responsible for creating, listing and deleting pastes
 const (
-	API_LOGIN = "https://pastebin.com/api/api_login.php"
-	API_POST = "https://pastebin.com/api/api_post.php"
+	APILogin = "https://pastebin.com/api/api_login.php"
+	APIPost  = "https://pastebin.com/api/api_post.php"
 )
 
+// User structure contains information about the pastebin user(username, password) and methods to generate the api user key
 type User struct {
-	apiKey string
+	apiKey   string
 	username string
 	password string
-	userKey string
-	api_option string
+	userKey  string
 }
 
+// GenerateKey method generates the user api key, which is used to create private pastes and to list user's pastes
 func (u *User) GenerateKey() (string, error) {
 
-	data_values := url.Values{
-		"api_dev_key": {u.apiKey},
-		"api_user_name":  {u.username},
+	dataValues := url.Values{
+		"api_dev_key":       {u.apiKey},
+		"api_user_name":     {u.username},
 		"api_user_password": {u.password},
 	}
-	data, httpResponseError := http.PostForm(API_LOGIN, data_values)
+	data, httpResponseError := http.PostForm(APILogin, dataValues)
 
 	if httpResponseError != nil {
 		return "", httpResponseError
 	}
-	
+
 	if data.StatusCode != 200 {
 		return "", errors.New("Couldn't generate the key: " + strconv.Itoa(data.StatusCode))
 	}
 
 	defer data.Body.Close()
-	
+
 	returnedData, err := ioutil.ReadAll(data.Body)
 	if err != nil {
 		return "", err
 	}
 
 	u.userKey = string(returnedData)
-	return u.userKey, nil	
+	return u.userKey, nil
 }
 
-
+// List method, lists the pastes of a user
 func (u *User) List(limit int) (string, error) {
 
 	apiOption := "list"
-	data_values := url.Values{
-		"api_dev_key": {u.apiKey},
-		"api_user_key": {u.userKey},
-		"api_option": {apiOption},
+	dataValues := url.Values{
+		"api_dev_key":       {u.apiKey},
+		"api_user_key":      {u.userKey},
+		"api_option":        {apiOption},
 		"api_results_limit": {strconv.Itoa(limit)},
 	}
-	data, httpResponseError := http.PostForm(API_POST, data_values)
+	data, httpResponseError := http.PostForm(APIPost, dataValues)
 
 	if httpResponseError != nil {
 		return "", httpResponseError
 	}
-	
+
 	if data.StatusCode != 200 {
 		return "", errors.New("Couldn't list the pastes: " + strconv.Itoa(data.StatusCode))
 	}
 
 	defer data.Body.Close()
-	
+
 	returnedData, err := ioutil.ReadAll(data.Body)
 	if err != nil {
 		return "", err
 	}
 
-	return string(returnedData), nil		
+	return string(returnedData), nil
 }
